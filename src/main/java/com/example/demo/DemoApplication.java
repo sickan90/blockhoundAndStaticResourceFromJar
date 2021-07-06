cc56ae9acdd65ce949ea65a8e9c2d2a0dc275573
Erik Sikander <erik.sikander@consultant.volvofinans.se>
2021-07-06 10:28:31 +0200

    Init Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.blockhound.BlockHound;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        BlockHound.install();
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
